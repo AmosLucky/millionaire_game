a0c7eba1cf6a0db140a20d2deac936772c1eba29
Amos Lucky <aluckya100@gmail.com>
2023-03-23 17:01:26 +0100

    EDIITED BUILD.GRADLE FOR NEW VERSION

--- app/src/main/java/com/digitalDreams/millionaire_game/DBHelper.java
+++ app/src/main/java/com/digitalDreams/millionaire_game/DBHelper.java
@@ -127,11 +127,11 @@ class DBHelper extends SQLiteOpenHelper {
             String selectQuery2 = "SELECT * FROM " + JSON_TABLE + " where  STAGE = " +current_play_level+ " ORDER BY RANDOM() LIMIT 1";
 
             Cursor res2 = db.rawQuery(selectQuery2,null);
-            db.close();
+            res2.close();
             return  res2;
 
         }
-        db.close();
+        res.close();
 
 
         return res;
@@ -144,7 +144,10 @@ class DBHelper extends SQLiteOpenHelper {
 
         Cursor res = db.rawQuery(selectQuery,null);
 
+
+        res.close();
         return res;
+
 
     }
 
